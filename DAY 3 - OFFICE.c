bedbd4e144c33f4af509499bcc0bf33f8621bdcf
saxena-693 <145220825+saxena-693@users.noreply.github.com>
2024-09-30 21:43:05 +0530

    Add files via upload

---
+++ DAY 3 - OFFICE.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+	int a,b,n;
+	scanf("%d",&n);
+	while(n!=0){
+	    scanf("%d" "%d", &a,&b);
+	    int temp = (a*4)+b;
+	    printf("%d\n",temp);
+	    n--;
+	}
+
+}
+
