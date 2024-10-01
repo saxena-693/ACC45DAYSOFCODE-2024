87a875823718a898c224a61498a2830ac6ca0099
saxena-693 <145220825+saxena-693@users.noreply.github.com>
2024-10-01 15:11:08 +0530

    Add files via upload

---
+++ DAY 4 - THREETOPICS.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int a,b,c,d;
+    scanf("%d %d %d %d",&a,&b,&c,&d);
+    if(d==a || d==b || d==c)
+    {
+        printf("Yes");
+        
+    }else {
+        printf("No");
+    }
+    
+
+}
+
