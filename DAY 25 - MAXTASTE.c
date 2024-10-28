5cec7422829eb0ee19bc3275bea3363e3d74a315
saxena-693 <145220825+saxena-693@users.noreply.github.com>
2024-10-28 11:43:23 +0530

    Add files via upload

---
+++ DAY 25 - MAXTASTE.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+    int t;
+	scanf("%d",&t);
+	while(t--){
+	    int a,b,c,d,x,y;
+	  scanf("%d%d%d%d",&a,&b,&c,&d);
+	     if(a>b)
+	    x=a;
+	        else
+	        x=b;
+	    
+	    if(c>d)
+	      y=c;  
+	    
+	    else
+	    y=d;
+	        printf("%d\n",x+y);
+	    
+	}
+	return 0;
+
+
+}
+
