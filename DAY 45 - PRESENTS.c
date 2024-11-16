3d8248e2872e378babfcd013de7cc0241c861fc9
saxena-693 <145220825+saxena-693@users.noreply.github.com>
2024-11-16 19:55:03 +0530

    Add files via upload

---
+++ DAY 45 - PRESENTS.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+	    int t;
+	    scanf("%d", &t);
+	    while(t!=0)
+	    {
+	        int n;
+	        scanf("%d", &n);
+	        printf("%d \n",(n-(n/5)));
+	        t--;
+	        
+	    }
+	return 0;
+
+}
+
