4339882f8315a502e8af0657f59a0717de6babbd
saxena-693 <145220825+saxena-693@users.noreply.github.com>
2024-11-24 14:16:09 +0530

    Add files via upload

---
+++ DAY 53 - DECINC.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+	    int n;
+    scanf("%d", &n);
+    if(n%4==0)
+    {
+        printf("%d \n", (n+1));
+    }
+    else
+    printf("%d \n", (n-1));
+    
+		return 0;
+}
+
